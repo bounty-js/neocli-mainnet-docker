be7867d9fdd8140d433a5e9e14052193fd141ba4
panther <bharatteja.6697@gmail.com>
2018-09-03 09:44:11 +0530

    check for CURRENT file

--- download_chain_and_run_neo_cli.sh
+++ download_chain_and_run_neo_cli.sh
@@ -1,7 +1,11 @@
 #/bin/sh
-if [ ! -f /app/chain.acc.zip ]; then
-    echo "Downloading Chain Data!"
-    wget http://sync.ngd.network/mainnet/full/chain.acc.zip
+
+if [ ! -f /app/Chain_00746E41/CURRENT ]; then
+    if [ ! -f /app/chain.acc.zip ]; then
+        echo "Downloading Chain Data!"
+        wget http://sync.ngd.network/mainnet/full/chain.acc.zip
+        echo "Finished Downloading Chain Data"
+    fi
 fi
 
-dotnet neo-cli.dll /rpc 
+dotnet neo-cli.dll /rpc
